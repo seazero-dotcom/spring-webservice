663f242c69dc74e3cffab9743e9b087a715a8a8e
DESKTOP-F8F2D2U\hychoi <withbb96@gmail.com>
2022-08-22 16:32:12 +0900

    프로젝트 생성

---
+++ src/main/java/com/seazero/webservice/Application.java
@@ -0,0 +1,13 @@
+package com.seazero.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.seazero.webservice.Application.class, args);
+	}
+
+}
